103336304e999ddc930e6138df79eeef96630a09
Rudra Kafle <student.168413@worcesterschools.net>
2024-05-07 18:13:18 -0400

    finished

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-ctx.lineJoin = "round";
+const clearButton = document.querySelector(".clear-button")
+
+let {height, width} = canvas;
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+let hue = 0;
+const LINE_INCREASE = 25;
+
 ctx.lineCap = "round";
-ctx.lineWidth = 10;
-ctx.beginPath();
+ctx.lineJoin = "round";
+ctx.lineWidth = LINE_INCREASE;
+reset()
 
-let x = 0
-let y = 0
 
-function draw(endx, endy) {
-    if (x > 500 || endx > 500) {
-        x = 500;
-        endx = 500;
-    }
-    if (y > 500 || endy > 500) {
-        y = 500;
-        endy = 500;
-    }
+function reset() {
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(endx,endy);
+    ctx.lineTo(x, y);
     ctx.stroke();
-    x = endx;
-    y = endy;
 }
 
-function canvasMovement(e) {
-    if (e.key == "ArrowRight") {
-        draw(x+4, y)
-    } else if (e.key == "ArrowDown") {
-        draw(x, y+4)
-    } else if (e.key == "ArrowLeft") {
-        draw(x-4, y)
-    } else if (e.key == "ArrowUp") {
-        draw(x, y-4)
-    }
-}
+function changePosition(e) {
+    const {key} = e
+    hue += 2
+    
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
 
-window.addEventListener("keydown", canvasMovement);
+    switch (key) {
+        case ("ArrowUp"):
+            y -= LINE_INCREASE;
+            break;
+        case ("ArrowDown"):
+            y += LINE_INCREASE;
+            break;
+        case ("ArrowRight"):
+            x += LINE_INCREASE;
+            break;
+        case ("ArrowLeft"):
+            x -= LINE_INCREASE;
+            break;
+    };
+        
+        ctx.lineTo(x, y);
+        ctx.stroke();
+};
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, width, height);
+    hue = 0
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    reset()
+};
+
+window.addEventListener("keydown", changePosition);
+clearButton.addEventListener("click", clearCanvas)
